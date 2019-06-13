543c102d50ce56360483bd73b5ca8413826d872f
Alexandre Louf <contact@alexandrelouf.fr>
2019-06-13 16:07:44 +0200

    lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DistribService} from './service/distrib.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'correction';
+  amount = 0;
+
+  products: any = [
+    {id: 1, name: 'iPhone XS', price: 2520},
+    {id: 2, name: 'iMac 2018', price: 3652},
+    {id: 3, name: 'Casque Beats', price: 1296},
+    {id: 4, name: 'iPad 10s', price: 210},
+    {id: 5, name: 'Apple TV', price: 845},
+    {id: 6, name: 'Mac Mini', price: 1815},
+    {id: 7, name: 'iPod', price: 50},
+    {id: 8, name: 'Apple Watch', price: 1530},
+  ];
+
+  productSelected: number = null;
+
+  showInfo = false;
+
+  firstname = null;
+  lastname = null;
+  address = null;
+
+  constructor(
+      private distribService: DistribService
+  ) {
+
+  }
+
+  addPiece(piece) {
+    this.amount += piece;
+  }
+
+  buy() {
+    if (this.productSelected == null) {
+      alert('Selectionnez un produit !');
+      return;
+    }
+
+    const product = this.products.find(pr => pr.id === this.productSelected);
+
+    if (this.amount !== product.price ) {
+      alert('Veuillez faire l\'appoint');
+      return;
+    }
+
+    alert(product.name + ' a été choisi');
+    this.showInfo = true;
+
+  }
+
+  submitinfo() {
+    if (this.firstname.length < 2) {
+      alert('prenom trop court');
+    } else if (this.lastname.length < 2) {
+      alert('Nom trop court');
+    } else if (this.address.length < 10) {
+      alert('Addresse trop courte');
+    } else {
+      this.distribService.buy({
+        idProduct: this.productSelected,
+        amount: this.amount / 100,
+        firstname: this.firstname,
+        lastname: this.lastname,
+        address: this.address
+      }).subscribe(
+          (res: any) => {
+            // cas ok !
+            console.log(res);
+          },
+          (err: any) => {
+            // cas pas ok
+            console.error(err);
+          }
+      );
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmountEuroPipe } from './pipe/amount-euro.pipe';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmountEuroPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
